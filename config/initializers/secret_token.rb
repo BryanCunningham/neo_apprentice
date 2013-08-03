fb83aa583514d7a583e8c6554afa0b9049580217
BryanCunningham <bryan.cunningham@me.com>
2013-08-02 21:09:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NeoApprentic::Application.config.secret_key_base = '1d4713c14623a469ee3e72b5eee54cb79a6724f4c35d237bc62e5fb6a26347b3aec80a2e05a74e43331eea3c776abd7a40cb74aa08915e290b3e778be73f415b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NeoApprentic::Application.config.session_store :cookie_store, key: '_neo_apprentic_session'
